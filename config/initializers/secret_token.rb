49b80424933dc9c2a28fcb7e821674bfe81de0ea
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-08 23:31:37 +0900

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+NekotterV4::Application.config.secret_key_base = 'd8bac3371d97642fb748621da0de90bd48626acb18db29b2b96373bbebed7fc690b60d6511ddfa895d5f85202c177dc7eb293a970df17871e0cc967455439891'
