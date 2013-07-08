5dc8397544b2b9e1508fc2e18b165827f861fa34
Hidenori Miyoshi <miyohide@gmail.com>
2013-07-08 23:39:07 +0900

    rails g scaffold tweet body:text & rake db:migrate

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 NekotterV4::Application.routes.draw do
+  resources :tweets
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
